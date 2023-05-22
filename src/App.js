e70bc87a574fe21c7e259cf446abdc292b83fd5b
chetankochiyaniya <ckochiyaniya950@rku.ac.in>
2023-05-22 16:21:19 +0530

    mui navabr added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
    <>
-   <h1>Hello</h1>
+   <div className="App">
+   <Navbar/>
+   </div>
    </>
   );
 }
